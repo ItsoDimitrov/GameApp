6c911c20e5872e37742c457bf1dd9810655dd6bb
ItsoDimitrov <fffsssccc@abv.bg>
2019-05-19 16:59:05 +0300

    Directory changed

---
+++ src/Services/Tests/Tools/Sandbox/GameApp.Tools.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameApp.Tools.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
